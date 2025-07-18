809b97a10b8c61eaee32afe90843429fcd486ecd
anshul <anshultyagi55562@gmail.com>
2025-07-18 22:46:19 +0530

    New journal app using springboot

---
+++ src/main/java/com/example/journalapp/controller/JournalEntryController.java
@@ -0,0 +1,25 @@
+package com.example.journalapp.controller;
+
+import com.example.journalapp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @GetMapping("/abc")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+
+    }
+
+
+}
